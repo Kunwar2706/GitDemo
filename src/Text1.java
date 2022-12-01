71d5f647ff1054f365d998cff63979d018374bfd
Me <Me@DESKTOP-HM27A8M>
2022-12-01 14:04:29 +0530

    first commit

---
+++ src/Text1.java
@@ -0,0 +1,13 @@
+
+public class Text1 {
+	public static void main(String[] args) {
+String hung="yes";
+if(hung=="yes")
+	System.out.println("something eat");
+else
+	System.out.println("do work");
+	}
+
+}
+
+
