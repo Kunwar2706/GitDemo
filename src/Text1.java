073aeb61f855497bc4fe09c1fe0a86c618a1d4cc
Me <Me@DESKTOP-HM27A8M>
2022-12-01 14:53:11 +0530

    modification done

--- src/Text1.java
+++ src/Text1.java
@@ -2,7 +2,7 @@
 public class Text1 {
 	public static void main(String[] args) {
 String hung="yes";
-if(hung=="yes")
+if(hung=="yes"||hung=="no")
 	System.out.println("something eat");
 else
 	System.out.println("do work");
